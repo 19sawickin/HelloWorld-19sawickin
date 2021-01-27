13d3036da8e0e421f02e10c100f0336eb5957179
Will Buerger <59492746+wbuerger46@users.noreply.github.com>
2021-01-27 17:05:48 -0500

    Initial commit

---
+++ App.java
@@ -0,0 +1,25 @@
+package helloworld;
+
+/**
+ * Welcome to the App class! This is where everything begins.
+ */
+public class App {
+
+  /**
+   * When you run the program, this "main" method gets called first automatically. You'll
+   * see that everything starts from this method!
+   */
+  public static void main(String[] args) {
+    // This line prints a greeting into the console
+    System.out.println("Hello world!");
+
+    // Here we make a new instance of Chef, and we store it in a variable
+    // named kevin. Hop over to Chef.java to see what's going on there.
+    Chef kevin = new Chef("Kevin");
+
+    // TODO: Get kevin to make chili and make a new Chef named Jim here!
+
+    // This line exits the program.
+    System.exit(0);
+  }
+}
---
+++ Chef.java
@@ -0,0 +1,31 @@
+package helloworld;
+
+/**
+ * This is the Chef class, which knows how to make food.
+ */
+public class Chef {
+
+  /**
+   * The constructor, which gets called whenever a new instance of Chef is made.
+   */
+  public Chef(String name) {
+    System.out.println("A new Chef named " + name + " has been made.");
+
+    // We want every instance of Chef to be ready to make food! That's why we
+    // call this method in the constructor.
+    this.getReady();
+  }
+
+  /**
+   * This method prints out this chef getting ready. Don't worry about the
+   * "private" keyword for now -- we'll come back to it later in class.
+   */
+  private void getReady() {
+    System.out.println("Washing hands, putting on an apron, and prepping the " +
+        "kitchen...");
+  }
+
+  /*
+   * TODO: Write your makeFood() method goes below!
+   */
+}
