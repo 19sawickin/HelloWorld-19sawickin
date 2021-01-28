36d5208e3629ebc93a1e806794a4542e3104eff0
Nick Sawicki <19sawickin@nicks-air-3.devices.brown.edu>
2021-01-27 20:10:27 -0500

    Finished Question 3

--- Chef.java
+++ Chef.java
@@ -28,4 +28,11 @@ public class Chef {
   /*
    * TODO: Write your makeFood() method goes below!
    */
+
+  public void makeFood(String foodToMake) {                 //should I use "void"? Or nothing? Error if nothing
+    System.out.println("Making" + foodToMake + "!");
+  }
+
+
 }
+
